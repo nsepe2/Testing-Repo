9523ab9681e9649b729b0162b1ff479f8a6c2a67
nsepe2 <133825386+nsepe2@users.noreply.github.com>
2024-12-04 18:52:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -85,12 +85,13 @@ if st.session_state.page == "seller":
             neighborhood_sentiment, host_neighborhood_sentiment, amenities_sentiment
         ] + encode_property_type(property_type)])
         predicted_score = model.predict(input_features)[0]
-        predicted_score = round(min(max(predicted_score, 0), 5), 2)
-        st.markdown(f"## 🔥 **Predicted Review Score Rating: {predicted_score:.2f} / 5** 🔥")
+        predicted_score = round(predicted_score, 2)
+        st.markdown(f"## 🔥 **Predicted Review Score Rating: {predicted_score:.2f}** 🔥")
 
 # Back button to go back to main page
 if st.button("Back"):
     st.session_state.page = "main"
+
 
 
 
