716497843d75ad936d77d85d99a69630dacc7979
nsepe2 <133825386+nsepe2@users.noreply.github.com>
2024-12-13 15:18:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -306,25 +306,25 @@ def main():
         # Reorder columns to match the expected features
         input_data_encoded = input_data_encoded[expected_features]
 
-        # Add button to submit input data
-        if st.button("Predict Review Score"):
-            # Standardize features
-            try:
-                input_data_scaled = scaler.transform(input_data_encoded)
-            except ValueError as e:
-                st.error(f"Error during feature scaling: {e}")
-                st.stop()
+      # Add button to submit input data
+if st.button("Predict Review Score"):
+    predicted_score = None  # Initialize the variable to avoid scope issues
+    try:
+        # Standardize features
+        input_data_scaled = scaler.transform(input_data_encoded)
+        # Make prediction
+        predicted_score = model.predict(input_data_scaled)[0]
 
-            # Make prediction
-            predicted_score = model.predict(input_data_scaled)[0]
-
-            # Check if predicted score is greater than 5
+        # Check if predicted score is greater than 5
         if predicted_score > 5:
             st.error("Uh-Oh, it seems that the combination of your inputs is very unique and our model is having trouble taking into account this unique combo. Try something else!")
         else:
             st.subheader("Predicted Review Score")
             st.markdown(f"<h2 style='font-size: 36px; color: #FF5733; font-weight: bold;'>The predicted review score for your listing is: {predicted_score:.2f}</h2>", 
-                    unsafe_allow_html=True)
+                        unsafe_allow_html=True)
+    except ValueError as e:
+        st.error(f"Error during feature scaling or prediction: {e}")
+
             
              # Footer
     st.markdown("""
